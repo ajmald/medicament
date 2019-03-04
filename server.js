a2a244365d32148eaea7997e7b3b4443ef74e0fc
ajmald <ajmaldookhan@gmail.com>
2019-03-04 12:21:58 +0100

    Compressing on server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
+
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/medicamentApp'));
+app.use(compression());
 
 app.get('/*', function(req,res) {
     
